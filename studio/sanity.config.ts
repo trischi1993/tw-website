642900e5b762ff6883ded7a99b556d2b986636c4
Tristan Weithaler <tristanweithaler@MacBook-Air-2.local>
2026-08-28 09:14:45 +0200

    Make Studio deployment self-contained

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -56,7 +56,7 @@ export default defineConfig({
   name: 'tristanweithaler',
   title: 'Tristan Weithaler',
 
-  projectId: projectId || 'missing-project-id',
+  projectId: projectId || '45zc9nhz',
   dataset,
 
   plugins: [
